1a39e9545d435b2b97731600bf403c1e7ef46b0c
Graham Mc <graham20@gmail.com>
2018-03-23 14:49:53 +0900

    Update LsdRadixSortTests.java

--- PrincetonA/src/com/pekoto/test/algorithms/LsdRadixSortTests.java
+++ PrincetonA/src/com/pekoto/test/algorithms/LsdRadixSortTests.java
@@ -1,5 +1,32 @@
 package com.pekoto.test.algorithms;
 
+/**
+ * Unit tests for the LsdRadixSort class.
+ */
 public class LsdRadixSortTests {
-
+	
+  @Test
+	public void testSorts() {
+		
+		String [] arr = {
+			"abe",
+			"ace",
+			"aba",
+			"bbd",
+			"gzi",
+			"bad",
+			"zap",
+			"oia"
+		};
+		
+		LsdRadixSort.sort(arr, 3);
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		for(int i = 1; i < arr.length; i++) {
+			assertTrue(arr[i-1].compareTo(arr[i]) <= 0);
+		}
+	}
 }
