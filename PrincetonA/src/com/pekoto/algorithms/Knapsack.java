06af9e51816e678b8f793245bba7417b172bd943
pekoto <graham20@gmail.com>
2018-04-03 21:25:33 +0900

    Updated comment

--- PrincetonA/src/com/pekoto/algorithms/Knapsack.java
+++ PrincetonA/src/com/pekoto/algorithms/Knapsack.java
@@ -22,6 +22,17 @@ package com.pekoto.algorithms;
  *      (left over capacity = row above, capacity-weight of this item)
  * 5. Final max value will be in the lower right of the matrix
  * 
+ * Matrix visualization:
+ * 
+ *                     Capacity
+ *                      0 1 2 3 4 5 6 7  8
+ *  Value Weight Item
+ *    0      0    0     0 0 0 0 0 0 0 0  0
+ *    4      2    1     0 0 4 4 4 4 4 4  4
+ *    2      4    2     0 0 4 4 4 4 6 6  6
+ *    1      1    3     0 1 4 5 5 5 5 7  7
+ *    5      4    4     0 1 5 5 5 5 5 10 10
+ *    1      7    5     0 1 5 5 5 5 5 10 10
  */
 public class Knapsack {
 
