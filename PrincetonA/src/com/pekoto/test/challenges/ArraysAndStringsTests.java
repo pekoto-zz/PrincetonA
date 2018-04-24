4c011151ee9b3016f3c373144b13417d1e7a519e
Graham Mc <graham20@gmail.com>
2018-04-24 15:14:48 +0900

    Update ArraysAndStringsTests.java

--- PrincetonA/src/com/pekoto/test/challenges/ArraysAndStringsTests.java
+++ PrincetonA/src/com/pekoto/test/challenges/ArraysAndStringsTests.java
@@ -39,4 +39,139 @@ public class ArraysAndStringsTests {
         assertTrue(ArraysAndStrings.oneAway("pale", "bale"));
         assertFalse(ArraysAndStrings.oneAway("pale", "bake"));
     }
+    
+    @Test
+	public void testZeroOut() {
+		int [][] matrix = {
+				{1,  2,  3,  4},
+				{5,  6,  7,  8},
+				{9,  10, 0,  12},
+				{13, 0,  15, 16},
+				{17, 18, 19, 20}
+		};
+		
+		StringsAndArrays.zeroOut(matrix);
+		
+		// Expected:
+		// 1  0 0 4
+		// 5  0 0 8
+		// 0  0 0 0
+		// 0  0 0 0
+		// 17 0 0 20
+		
+		assertEquals(1, matrix[0][0]);
+		assertEquals(0, matrix[0][1]);
+		assertEquals(0, matrix[0][2]);
+		assertEquals(4, matrix[0][3]);
+		
+		assertEquals(5, matrix[1][0]);
+		assertEquals(0, matrix[1][1]);
+		assertEquals(0, matrix[1][2]);
+		assertEquals(8, matrix[1][3]);
+		
+		assertEquals(0, matrix[2][0]);
+		assertEquals(0, matrix[2][1]);
+		assertEquals(0, matrix[2][2]);
+		assertEquals(0, matrix[2][3]);
+		
+		assertEquals(0, matrix[3][0]);
+		assertEquals(0, matrix[3][1]);
+		assertEquals(0, matrix[3][2]);
+		assertEquals(0, matrix[3][3]);
+		
+		assertEquals(17, matrix[4][0]);
+		assertEquals(0, matrix[4][1]);
+		assertEquals(0, matrix[4][2]);
+		assertEquals(20, matrix[4][3]);
+	}
+	
+	@Test
+	public void testZeroOut_ZeroInFirstRow() {
+		int [][] matrix = {
+				{1,  2,  3,  0},
+				{5,  6,  7,  8},
+				{9,  10, 0,  12},
+				{13, 0,  15, 16},
+				{17, 18, 19, 20}
+		};
+		
+		StringsAndArrays.zeroOut(matrix);
+		
+		// Expected:
+		// 0  0 0 0
+		// 5  0 0 0
+		// 0  0 0 0
+		// 0  0 0 0
+		// 17 0 0 0
+		
+		assertEquals(0, matrix[0][0]);
+		assertEquals(0, matrix[0][1]);
+		assertEquals(0, matrix[0][2]);
+		assertEquals(0, matrix[0][3]);
+		
+		assertEquals(5, matrix[1][0]);
+		assertEquals(0, matrix[1][1]);
+		assertEquals(0, matrix[1][2]);
+		assertEquals(0, matrix[1][3]);
+		
+		assertEquals(0, matrix[2][0]);
+		assertEquals(0, matrix[2][1]);
+		assertEquals(0, matrix[2][2]);
+		assertEquals(0, matrix[2][3]);
+		
+		assertEquals(0, matrix[3][0]);
+		assertEquals(0, matrix[3][1]);
+		assertEquals(0, matrix[3][2]);
+		assertEquals(0, matrix[3][3]);
+		
+		assertEquals(17, matrix[4][0]);
+		assertEquals(0, matrix[4][1]);
+		assertEquals(0, matrix[4][2]);
+		assertEquals(0, matrix[4][3]);
+	}
+	
+	@Test
+	public void testZeroOut_ZeroInFirstCol() {
+		int [][] matrix = {
+				{1,  2,  3,  4},
+				{5,  6,  7,  8},
+				{9,  10, 0,  12},
+				{13, 0,  15, 16},
+				{0, 18, 19, 20}
+		};
+		
+		StringsAndArrays.zeroOut(matrix);
+		
+		// Expected:
+		// 0 0 0 4
+		// 0 0 0 8
+		// 0 0 0 0
+		// 0 0 0 0
+		// 0 0 0 0
+		
+		assertEquals(0, matrix[0][0]);
+		assertEquals(0, matrix[0][1]);
+		assertEquals(0, matrix[0][2]);
+		assertEquals(4, matrix[0][3]);
+		
+		assertEquals(0, matrix[1][0]);
+		assertEquals(0, matrix[1][1]);
+		assertEquals(0, matrix[1][2]);
+		assertEquals(8, matrix[1][3]);
+		
+		assertEquals(0, matrix[2][0]);
+		assertEquals(0, matrix[2][1]);
+		assertEquals(0, matrix[2][2]);
+		assertEquals(0, matrix[2][3]);
+		
+		assertEquals(0, matrix[3][0]);
+		assertEquals(0, matrix[3][1]);
+		assertEquals(0, matrix[3][2]);
+		assertEquals(0, matrix[3][3]);
+		
+		assertEquals(0, matrix[4][0]);
+		assertEquals(0, matrix[4][1]);
+		assertEquals(0, matrix[4][2]);
+		assertEquals(0, matrix[4][3]);
+	}
 }
